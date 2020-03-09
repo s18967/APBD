9586c91b9384ecebe8a6826124f1dbdbd85274bc
Bartosz Dąbrowski <s18967@pjwstk.edu.pl>
2020-03-09 10:09:24 +0100

    Serwis

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -7,34 +7,62 @@ namespace cw2
 {
     class Program
     {
+        static void loggingErrors(Exception ex)
+        {
+            string logpath = @"C:\Users\s18967\Desktop\APBD\cw2";
+            if (!File.Exists(logpath))
+            {
+                File.Create(logpath).Dispose(); //Dispose - doczytaj
+            }
+            using (StreamWriter s = File.AppendText(logpath))
+            {
+                s.WriteLine("Start " + DateTime.Now);
+                s.WriteLine("Blad: " + ex.Message);
+            }
+        }
+
         static void Main(string[] args)
         {
-            String csvpath = Console.ReadLine(); //C:\Users\s18967\Desktop\APBD\dane.csv
-            String xmlpath = Console.ReadLine(); //C:\Users\s18967\Desktop\APBD
-            String format = Console.ReadLine();  //xml
+            try {
+                String csvpath = Console.ReadLine(); //C:\Users\s18967\Desktop\APBD\cw2\dane.csv
+                String xmlpath = Console.ReadLine(); //C:\Users\s18967\Desktop\APBD\cw2
+                String format = Console.ReadLine();  //xml
 
-            String[] source = File.ReadAllLines(csvpath);
+                if (File.Exists(csvpath) && Directory.Exists(xmlpath))
+                {
 
-            XElement xml = new XElement("Uczelnia",
-             from str in source
-             let fields = str.Split(',')
-             select new XElement("Studenci",
-                    new XAttribute("indexNumber", "s"+fields[4]),
-                    new XElement("fname", fields[0]),
-                    new XElement("lname", fields[1]),
-                    new XElement("birthdate", fields[5]),
-                    new XElement("email", fields[6]),
-                    new XElement("mothersName", fields[7]),
-                    new XElement("fathersName", fields[8]),
-                    new XElement("studies",
-                        new XElement("name", fields[2]),
-                        new XElement("mode", fields[3])
-                    )
-                )
-             );
+                    String[] source = File.ReadAllLines(csvpath);
 
-            xml.Save(String.Concat(xmlpath + "result.xml")); // konkatenacja pozwala nam nazwac plik
+                    XElement xml = new XElement("Uczelnia",
+                     from str in source
+                     let fields = str.Split(',')
+                     select new XElement("Studenci",
+                            new XAttribute("indexNumber", "s" + fields[4]),
+                            new XElement("fname", fields[0]),
+                            new XElement("lname", fields[1]),
+                            new XElement("birthdate", fields[5]),
+                            new XElement("email", fields[6]),
+                            new XElement("mothersName", fields[7]),
+                            new XElement("fathersName", fields[8]),
+                            new XElement("studies",
+                                new XElement("name", fields[2]),
+                                new XElement("mode", fields[3])
+                            )
+                        )
+                     );
 
+                    xml.Save(String.Concat(xmlpath + "result.xml")); // konkatenacja pozwala nam nazwac plik
+                }
+                else
+                {
+                    if (!File.Exists(csvpath))
+                    {
+                        throw new Exception("wrong path to csv file");
+                    }
+                }
+            } catch (Exception ex) {
+                loggingErrors(ex);
+            }
         }
     }
 }
