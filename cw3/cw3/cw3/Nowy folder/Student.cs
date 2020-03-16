e0f93ec545f182814027c68bf5923b3190c115c0
Bartek Dabrowski <bartosz.dabrowskini@gmail.com>
2020-03-16 10:23:34 +0100

    Cwiczenia 3

---
+++ cw3/cw3/cw3/Nowy folder/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.cw3.Nowy_folder
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+    
+        public string LastName { get; set; }
+
+        public string IndexNumber { get; set; }
+    }
+}
