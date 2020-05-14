c9bc0cb98d1597bbca95970ea06cd18e87eefeb5
Bartek Dabrowski <bartosz.dabrowskini@gmail.com>
2020-05-14 16:25:14 +0200

    cw10 blank

---
+++ cw10/cw10/Models/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cw10.Models
+{
+    public partial class Doctor
+    {
+        public Doctor()
+        {
+            Prescription = new HashSet<Prescription>();
+        }
+
+        public int IdDoctor { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public virtual ICollection<Prescription> Prescription { get; set; }
+    }
+}
---
+++ cw10/cw10/Models/PrescriptionMedicament.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cw10.Models
+{
+    public partial class PrescriptionMedicament
+    {
+        public int IdMedicament { get; set; }
+        public int IdPrescription { get; set; }
+        public int Dose { get; set; }
+        public string Details { get; set; }
+
+        public virtual Medicament IdMedicamentNavigation { get; set; }
+        public virtual Prescription IdPrescriptionNavigation { get; set; }
+    }
+}
